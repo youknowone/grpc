9eea238a75374bf0912fdaab9d520e22343d3c20
Jeong YunWon <jeong@youknowone.org>
2020-12-12 03:52:10 +0900

    Fix grpc++/impl/method_handler_impl.h
    to include method_handler.h
    
    For now, this file is including grpcpp/impl/method_handler_impl.h,
    which is deprecated and even not exposed as public itnerface.
    This causes not just deprecation warning but causes build error.
    
    This patch fix the redirection target to the correct file to let it
    works until the users move to grpcpp headers.

--- include/grpc++/impl/codegen/method_handler_impl.h
+++ include/grpc++/impl/codegen/method_handler_impl.h
@@ -23,6 +23,4 @@
 #ifndef GRPCXX_IMPL_CODEGEN_METHOD_HANDLER_IMPL_H
 #define GRPCXX_IMPL_CODEGEN_METHOD_HANDLER_IMPL_H
 
-#include <grpcpp/impl/codegen/method_handler_impl.h>
-
 #endif  // GRPCXX_IMPL_CODEGEN_METHOD_HANDLER_IMPL_H
